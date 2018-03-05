aa9b8faa905745f7beb9f5ea3455eb20328ec50d
Andreas Frohwerk <andreas.frohwerk@gmail.com>
2018-03-05 09:53:41 +0100

    Initial commit

---
+++ CSV_Fusion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unist.h>
+#include <math.h>
+
+FILE *ifp;
+char *mode = "rt";
+
+while (fscanf(ifp, "%f;%f;%f;%d;%d;%d", &x, &y, &z, &r, &g, &b) != EOF) {
+
+}
---
+++ listing-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unist.h>
+#include <math.h>
+
+float x,y,z;
+int r,g,b;
+
+FILE *ifp;
+char *mode = "rt";
+
+while (fscanf(ifp, "%f;%f;%f;%d;%d;%d", &x, &y, &z, &r, &g, &b) != EOF) {
+
+}
